e3e72b80e17170eb47b3dea4d67880dfc70c601f
Yannis-Borel <yannis.borel@edu.univ-fcomte.fr>
2024-11-26 08:50:06 +0100

    feat(Homeview mise a jour)

--- PROJET_YANNIS_BOREL/src/views/HomeView.vue
+++ PROJET_YANNIS_BOREL/src/views/HomeView.vue
@@ -1,17 +1,35 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
-
 import Form from '@/components/Form.vue';
-
 </script>
 
 <template>
-
-   
-
-    <h1>Home</h1>
-      <Form>fefefefeffefefefe</Form>
-
-
-
+  <div class="home">
+    <div class="home__logo-container">
+      <img src="@/assets/image/logo.png" alt="Logo" class="home__logo" />
+    </div>
+    <Form>fefefefeffefefefe</Form>
+  </div>
 </template>
+
+<style lang="scss">
+.home {
+  min-height: 100vh; // Assure que le fond couvre tout l'écran
+  background: linear-gradient(135deg, $primaryColor, $primaryDarkColor); // Dégradé
+  color: #fff; // Texte blanc pour contraster avec le fond
+  display: grid;
+  place-items: center;
+
+  &__logo-container {
+    display: grid;
+    place-items: center;
+    width: 100%;
+  }
+
+  &__logo {
+    max-width: 100%;
+    width: auto; // Ajuste la taille de ton logo
+    height: auto;
+  }
+}
+</style>
