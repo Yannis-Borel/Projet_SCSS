e0b75972162f8e53ec17cc172897887b7e35ae14
Yannis-Borel <yannis.borel@edu.univ-fcomte.fr>
2024-12-02 13:51:28 +0100

    feat(Layout): Layout + slot

---
+++ pages/admin.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+definePageMeta({
+    layout: 'minimal'
+})
+</script>
+
+<template>
+    <div class="p-admin">
+        admin
+    </div>
+</template>
+
+<style lang="scss" >
+</style>
