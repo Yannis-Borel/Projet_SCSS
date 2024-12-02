a02ee1720a909447f891074ecbddf59b8b049a81
Yannis-Borel <yannis.borel@edu.univ-fcomte.fr>
2024-12-02 09:17:12 +0100

    feat(install_nuxt): Installation

--- vuejs/src/components/RegisterForm.vue
+++ vuejs/src/components/RegisterForm.vue
@@ -68,9 +68,7 @@ function handleSubmit() {
   gap: 1.5rem;
   margin: 0 auto;
   padding: 2rem;
-  background-color: #f9f9f9;
-  border-radius: 8px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
 
   &__group {
     display: flex;
