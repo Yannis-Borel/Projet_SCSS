8e6505a489cbc91aa6a9076fb1249f3bae5c9378
Yannis-Borel <yannis.borel@edu.univ-fcomte.fr>
2024-12-04 08:35:21 +0100

    feat(sanity): Avancement sur les categories

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,33 +1,42 @@
 <script setup lang="ts">
 
 import type { SanityDocument } from "@sanity/client";
-const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
+
+// Inclure la catégorie dans la requête
+const POST_QUERY = groq`
+  *[_type == "post" && slug.current == $slug][0]{
+    title,
+    image,
+    body,
+    "category": category->title 
+  }`
+;
+
 const route = useRoute();
 
-const {data: post} = await useSanityQuery<SanityDocument>(POST_QUERY, {slug: route.params.slug});
+const { data: post } = await useSanityQuery<SanityDocument>(POST_QUERY, { slug: route.params.slug });
 
+// Renvoyer une page d'erreur si le post n'existe pas
 if (!post.value) {
-    throw createError({
-        statusCode: 404
-    })
+  throw createError({ statusCode: 404, statusMessage: "Page not found" });
 }
-
-console.log(post);
-
 
 </script>
 
+
 <template>
-  <main>
-    <div v-if="post">
-        Nom du slug : {{ route.params.slug }}
-        <h1>{{ post.title }}</h1>
+    <div v-if="post" class="p-blog-slug">
+      <h1>{{ post.title }}</h1>
 
+      <!-- Afficher la catégorie -->
+      <p class="p-blog-category">Category: {{ post.category }}</p>
+<div v v-for="(category, index) in post.categories" :key="index ">
+  <p class="p-blog-category">Category titre: {{ category.title }}</p>
+</div>
+      <div class="p-blog-slug_image">
+        <SanityImage :asset-id="post.image.asset._ref" />
+      </div>
 
-        <SanityContent :blocks="post.body" />
-        <SanityImage v-if="post.image" :image="post.image.asset._ref" alt="l'image"/>
-
-
+      <SanityContent v-bind="{ blocks: post.body }" />
     </div>
-  </main>
-</template>
+  </template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -7,39 +7,37 @@ import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 const POSTS_QUERY = groq`*[
   _type == "post"
   && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, image, slug, publishedAt}`;
+]|order(publishedAt desc)[0...12]{_id, image, title, slug, publishedAt}`;
 
 const { data: posts } = await useSanityQuery<SanityDocument[]>(POSTS_QUERY);
 
 const { projectId, dataset } = useSanity().client.config();
+
+
 const urlFor = (source: SanityImageSource) =>
   projectId && dataset
     ? imageUrlBuilder({ projectId, dataset }).image(source)
     : null;
-
-definePageMeta({
-    layout: 'minimal'
-})
 </script>
 
+
 <template>
-    <main>
-        <div>
-            <h1>Blog</h1>
-                <ul>
-                    <li v-for="post in posts" :key="post._id">
-                        <NuxtLink :to="`/blog/${post.slug.current}`">
-                            <p>{{ post.title }}</p>
-                            <img v-if="post.image" :src="urlFor(post.image)?.url()" alt="" >
-                            <div v-else>No images</div>
-                        </NuxtLink>
+    <div class="p-blog">
 
-                    </li>
-                </ul>
-        </div>
-    </main>
+        une page de listing
+<div>
+    <ul>
 
-</template> 
+        <li v-for="(post, index) in posts" :key="index">
+            <NuxtLink :to="`/blog/${post.slug.current}`">
+                {{ post.title }}
+            </NuxtLink>
+            <img v-if="post.image" class="p-blog__item-image" :src="urlFor(post.image)?.url()" alt="post.title" >
+        </li>
+    </ul>
+    </div>
+</div>
+</template>
 
-<style lang = "scss">
+<style lang="scss" >
 </style>
