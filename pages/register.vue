fc7f8d56aa8c616e33f8e6c1797e5fdd1cdfd970
Yannis-Borel <yannis.borel@edu.univ-fcomte.fr>
2024-12-10 08:59:43 +0100

    feat(API_Register): Mise en place de la page connexion avec l'API

---
+++ pages/register.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+async function onSubmit(event: SubmitEvent) {
+    event.preventDefault()
+    console.log('form has submitted', { username: username.value, password: password.value })
+}
+</script>
+
+<template>
+    <div>
+        <h1>Page d'inscription</h1>
+        username : {{ username }}
+        password : {{ password }}
+
+        <form @submit="onSubmit">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" id="username" v-model="username">
+            </div>
+            <div>
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" v-model="password">
+            </div>
+            <div>
+                <button type="submit">S'inscrire</button>
+            </div>
+        </form>
+    </div>
+</template>
